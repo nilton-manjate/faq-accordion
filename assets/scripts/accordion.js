19fde0f85f59109e6de29cf4987d566cfdadb62d
nilton-manjate <ntnmanjate1@gmail.com>
2023-12-13 09:44:53 +0200

    solve double click issue to show the answer

--- assets/scripts/accordion.js
+++ assets/scripts/accordion.js
@@ -4,7 +4,9 @@ const question = document.getElementsByClassName("question");
 const answer = document.getElementsByClassName("answer");
 const icon = document.getElementsByClassName("icon");
 
-question[0].addEventListener("click", function () {
+answer[0].style.display = "none";
+icon[0].src = "assets/images/icon-plus.svg";
+function show0() {
   if (answer[0].style.display == "none") {
     answer[0].style.display = "block";
     icon[0].src = "assets/images/icon-minus.svg";
@@ -12,9 +14,14 @@ question[0].addEventListener("click", function () {
     answer[0].style.display = "none";
     icon[0].src = "assets/images/icon-plus.svg";
   }
-});
+}
+question[0].tabIndex = "1";
+question[0].addEventListener("click", show0);
+question[0].addEventListener("keydown", show0);
 
-question[1].addEventListener("click", function () {
+answer[1].style.display = "none";
+icon[1].src = "assets/images/icon-plus.svg";
+function show1() {
   if (answer[1].style.display == "none") {
     answer[1].style.display = "block";
     icon[1].src = "assets/images/icon-minus.svg";
@@ -22,9 +29,14 @@ question[1].addEventListener("click", function () {
     answer[1].style.display = "none";
     icon[1].src = "assets/images/icon-plus.svg";
   }
-});
+}
+question[1].tabIndex = "2";
+question[1].addEventListener("click", show1);
+question[1].addEventListener("keydown", show1);
 
-question[2].addEventListener("click", function () {
+answer[2].style.display = "none";
+icon[2].src = "assets/images/icon-plus.svg";
+function show2() {
   if (answer[2].style.display == "none") {
     answer[2].style.display = "block";
     icon[2].src = "assets/images/icon-minus.svg";
@@ -32,9 +44,14 @@ question[2].addEventListener("click", function () {
     answer[2].style.display = "none";
     icon[2].src = "assets/images/icon-plus.svg";
   }
-});
+}
+question[2].tabIndex = "3";
+question[2].addEventListener("click", show2);
+question[2].addEventListener("keydown", show2);
 
-question[3].addEventListener("click", function () {
+answer[3].style.display = "none";
+icon[3].src = "assets/images/icon-plus.svg";
+function show3() {
   if (answer[3].style.display == "none") {
     answer[3].style.display = "block";
     icon[3].src = "assets/images/icon-minus.svg";
@@ -42,5 +59,7 @@ question[3].addEventListener("click", function () {
     answer[3].style.display = "none";
     icon[3].src = "assets/images/icon-plus.svg";
   }
-});
-
+}
+question[3].tabIndex = "4";
+question[3].addEventListener("click", show3);
+question[3].addEventListener("keydown", show3);
